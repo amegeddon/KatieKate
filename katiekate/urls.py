975748b833db18e242b3ed73f15b6758b30968dc
amegeddon <ameescook@gmail.com>
2025-03-10 20:06:16 +0000

    Mapping media folder to urls.py and styling index.html with base.css

--- katiekate/urls.py
+++ katiekate/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -24,5 +26,5 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', views.index, name='home'),
-]
+    path('', include('home.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

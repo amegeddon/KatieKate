9b8adc58012f557622727f2c339f4d97b0262943
amegeddon <ameescook@gmail.com>
2025-03-11 15:22:28 +0000

    Create 'bag' app and implement shopping cart page rendering
    - Created a new 'bag' app to manage shopping cart functionality.
    - Added `bag.html` template for rendering the shopping cart page.
    - Linked `view_bag` view to render the `bag.html` template when the shopping cart icon is clicked.
    - Configured the URL routing for the 'bag' app to handle the `view_bag` view.
    - Updated the shopping cart icon in the header to correctly link to the shopping bag page.
    - Ensured that clicking on the cart icon redirects users to the `/bag/` URL and displays the shopping cart.

--- katiekate/urls.py
+++ katiekate/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from home import views
 from django.conf import settings
 from django.conf.urls.static import static
-
 
 
 
@@ -28,4 +26,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

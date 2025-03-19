959fb922c33e593daa5265e43d9aab76b3b0c598
amegeddon <ameescook@gmail.com>
2025-03-19 22:42:02 +0000

    Feat: Checkout Success page: - Added functionality to display order information upon successful checkout. - Added a button for users to explore new arrivals, deals, and clearance products.

--- katiekate/settings.py
+++ katiekate/settings.py
@@ -171,12 +171,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STANDARD_DELIVERY_PERCENTAGE = 10 #variable for delivery calculations 
-FREE_DELIVERY_THRESHOLD = 50 #delete this and adjust contexts.py appropiately 
+
 
 # Stripe
-FREE_DELIVERY_THRESHOLD = 50
+
 STANDARD_DELIVERY_PERCENTAGE = 10
-STRIPE_CURRENCY = 'usd'
+STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 

02d9690c16240bb3bd4f10eb319232b8a8ee1504
amegeddon <ameescook@gmail.com>
2025-03-20 15:04:17 +0000

    Completed webhook handler for Stripe payment processing, including order verification, creation, and handling of payment success and failure.

--- checkout/views.py
+++ checkout/views.py
@@ -47,66 +47,78 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
-
         order_form = OrderForm(form_data)
-
-        if order_form.is_valid(): 
-            order = order_form.save()
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=item_data,
-                    )
-                    order_line_item.save()
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your bag wasn't found in our database. "
-                        "Please call us for assistance!"
-                    ))
+                        "Please call us for assistance!")
+                    )
                     order.delete()
-                    return redirect(reverse('view_bag')) 
+                    return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
-
         else:
-            messages.error(request, 'There was an error with your form. Please double-check your information.')
-
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
-            bag = request.session.get('bag', {})
-            if not bag:
-                messages.error(request, "There's nothing in your bag at the moment")
-                return redirect(reverse('products'))
-            
-            current_bag = bag_contents(request)
-            total = current_bag['grand_total']
-            stripe_total = round(total * 100)
-            stripe.api_key = stripe_secret_key
-            intent = stripe.PaymentIntent.create(
-                amount=stripe_total,
-                currency=settings.STRIPE_CURRENCY,
-            )
-            
-            order_form = OrderForm()
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
 
-            # in the video, the below code is not indented properly
-            # this is the correct indentation
-            if not stripe_public_key:
-                messages.warning(request, 'Stripe public key is missing. \
-                    Did you forget to set it in your environment?')
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
-            template = 'checkout/checkout.html'
-            context = {
-                'order_form': order_form,
-                'stripe_public_key': stripe_public_key,
-                'client_secret': intent.client_secret,
-            }
+        order_form = OrderForm()
 
-            return render(request, template, context)
-            
+        # in the video, the below code is not indented properly
+        # this is the correct indentation
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+        return render(request, template, context)
+        # end of the corrected indentation
+
 
 def checkout_success(request, order_number):
     """
@@ -117,7 +129,7 @@ def checkout_success(request, order_number):
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
-    
+
     if 'bag' in request.session:
         del request.session['bag']
 
@@ -125,6 +137,5 @@ def checkout_success(request, order_number):
     context = {
         'order': order,
     }
-    return render(request, template, context)
 
-   
+    return render(request, template, context)

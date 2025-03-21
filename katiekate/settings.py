f41e81aa47107fb2877e87d87eef68561fa2b671
amegeddon <ameescook@gmail.com>
2025-03-21 17:38:46 +0000

    feat: Send order confirmation email via webhook
    Updated the Stripe webhook handler to send a confirmation email to customers after a successful payment.
    The `_send_confirmation_email` method is now triggered when an order is verified or created via the `payment_intent.succeeded` event.
    This ensures that customers receive an email confirmation even if there are issues redirecting them to the success page.

--- katiekate/settings.py
+++ katiekate/settings.py
@@ -182,6 +182,9 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+DEFAULT_FROM_EMAIL = 'KaiteKate@example.com'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

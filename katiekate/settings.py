78188605c646173940513ef1e5cd95e820eb2f34
amegeddon <ameescook@gmail.com>
2025-03-28 12:46:39 +0000

    Repopulates static folder

--- katiekate/settings.py
+++ katiekate/settings.py
@@ -14,8 +14,7 @@ import os
 import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
-    import envs
-
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

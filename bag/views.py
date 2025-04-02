914eb349b903236bc1fd7e40dd9216beba6cd938
amegeddon <ameescook@gmail.com>
2025-04-02 03:16:07 +0100

    correct code errors

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
-
 from products.models import Product
 
 # Create your views here.
-
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -32,16 +30,14 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of the specified product
-    to the specified amount """
+    """ Adjust the quantity of the specified product to the specified amount """
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
-        messages.success(request, f'Updated {
-            product.name} quantity to {bag[item_id]}')
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')  # Corrected line
     else:
         bag.pop(item_id, None)
         messages.success(request, f'Removed {product.name} from your bag')
